fc75d37f2922a63e03fac2b1709dbe210143405a
Henrique <78804989+henrique-coder@users.noreply.github.com>
2026-01-12 09:39:31 -0300

    Update launcher.ps1

--- scripts/launcher.ps1
+++ scripts/launcher.ps1
@@ -26,9 +26,12 @@ $iconPath = "$dataDir\icon.ico"
 $edgeIconUrl = "https://raw.githubusercontent.com/henrique-coder/correios-tools/refs/heads/dev/assets/logos/edge.png"
 $chromeIconUrl = "https://raw.githubusercontent.com/henrique-coder/correios-tools/refs/heads/dev/assets/logos/chrome.png"
 $selfUpdateUrl = "https://github.com/henrique-coder/correios-tools/releases/download/minified-scripts/launcher.dev.min.ps1"
-$extensionUrls = @("https://github.com/henrique-coder/correios-tools/releases/download/browser-extensions/sroweb_inducao.zip")
+$extensionUrls = @(
+        "https://github.com/henrique-coder/correios-tools/releases/download/browser-extensions/sroweb_inducao.zip",
+        "https://github.com/henrique-coder/correios-tools/releases/download/browser-extensions/sroweb_loecview_hud.zip"
+)
 $scriptUrls = @()
-$startUrl = "https://sroweb.correios.com.br/app/entregaexternaautomatica/lancamento/index.php"
+$startUrl = "https://sroweb.correios.com.br/app/index.php"
 
 if (!(Test-Path $dataDir)) { New-Item -ItemType Directory -Path $dataDir -Force | Out-Null }
 if (!(Test-Path $assetsDir)) { New-Item -ItemType Directory -Path $assetsDir -Force | Out-Null }
