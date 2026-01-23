a236cb2d35e2286692632babf426b52cd0485906
Henrique <78804989+henrique-coder@users.noreply.github.com>
2026-01-23 10:24:25 -0300

    remove: extensions/sroweb-induction directory

--- extensions/sroweb-induction/background.js
+++
@@ -1,5 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({
-    url: "https://sroweb.correios.com.br/app/entregaexternaautomatica/lancamentoautomatico/index.php",
-  });
-});
