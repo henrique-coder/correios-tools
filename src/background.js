478d6275719576b958d56c8cea436dfc4729fd10
Henrique <78804989+henrique-coder@users.noreply.github.com>
2026-02-11 13:29:31 +0000

    feat: add browser polyfill and update background and content scripts for compatibility

--- src/background.js
+++ src/background.js
@@ -1,5 +1,9 @@
+if (typeof browser === "undefined") {
+  var browser = chrome;
+}
+
 const SROWEB_INDEX_URL = "https://sroweb.correios.com.br/app/index.php";
 
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ url: SROWEB_INDEX_URL });
+browser.action.onClicked.addListener(() => {
+  browser.tabs.create({ url: SROWEB_INDEX_URL });
 });
--- src/content.js
+++ src/content.js
@@ -1,7 +1,9 @@
 const script = document.createElement("script");
-((script.src = chrome.runtime.getURL("injected.js")),
-  (script.type = "module"),
-  (script.onload = function () {
-    this.remove();
-  }),
-  (document.head || document.documentElement).appendChild(script));
+
+script.src = browser.runtime.getURL("injected.js");
+script.type = "module";
+script.onload = function () {
+  this.remove();
+};
+
+(document.head || document.documentElement).appendChild(script);
