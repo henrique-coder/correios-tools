f3cfcd99c840129e08a1c9847e14db8b8163516e
Henrique Moreira <github@henriquecoder.com>
2026-04-06 13:28:28 +0000

    feat: add devcontainer configuration and setup script for development environment

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+sudo rm -f /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update -yqq
+sudo apt-get upgrade -yqq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+
+sudo apt-get install -yqq build-essential btop
